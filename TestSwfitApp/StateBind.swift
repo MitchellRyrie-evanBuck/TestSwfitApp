4ec5834bd1124eee27a02c4b2ad300f1a5459ca7
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-08-16 23:53:44 +0800

    feat: state
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

--- TestSwfitApp/StateBind.swift
+++ TestSwfitApp/StateBind.swift
@@ -18,6 +18,41 @@ struct ContentStateBindView: View {
         Text("The switch is on!")
       }
     }
+      ContentStateDataView()
     .padding()
+  }
+}
+
+struct ContentStateDataView: View {
+  @State private var selectedColor = Color.red
+
+  var body: some View {
+    VStack {
+      Rectangle()
+        .fill(selectedColor)
+        .frame(width: 100, height: 100, alignment: .center)
+
+      ColorPickerView(selectedColor: $selectedColor)
+    }
+    .padding()
+  }
+}
+
+struct ColorPickerView: View {
+  @Binding var selectedColor: Color
+
+  let colors: [Color] = [.red, .green, .blue, .yellow, .orange]
+
+  var body: some View {
+    HStack {
+      ForEach(colors, id: \.self) { color in
+        Rectangle()
+          .fill(color)
+          .frame(width: 50, height: 50)
+          .onTapGesture {
+            selectedColor = color
+          }
+      }
+    }
   }
 }
