475501a0153e114ddc6ab716d4106a4e82920073
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-09-16 22:56:45 +0800

    feat: 目录更改
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

--- TestSwfitApp/Views/Anmation.swift
+++ TestSwfitApp/Views/Anmation.swift
@@ -6,13 +6,36 @@
 //
 
 import SwiftUI
-
-struct Anmation: View {
+let examplesField: [Example] = [
+      Example(name: "网格测试", destination: AnyView(FieldContentView())),
+   
+  ]
+// 其他页面的示例
+struct AnmationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {  // 添加 ScrollView 以允许滚动
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3)) {
+                    ForEach(examplesField) { example in
+                        NavigationLink(destination: example.destination) {
+                            VStack {
+                                Text(example.name)
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 100)
+                                    .background(.blue)
+                                    .modifier(BoldAndItalicModifier())
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("示例列表") // 如果需要，可以为 NavigationView 添加标题
+        }
     }
 }
 
+
 #Preview {
-    Anmation()
+    AnmationView()
 }
--- TestSwfitApp/Views/MainView.swift
+++ TestSwfitApp/Views/MainView.swift
@@ -7,12 +7,44 @@
 
 import SwiftUI
 
-struct MainView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct Example: Identifiable {
+    let id = UUID()
+    let name: String
+    let destination: AnyView
+}
+
+let examples: [Example] = [
+      Example(name: "网格测试", destination: AnyView(ContentGridView())),
+      Example(name: "表单测试", destination: AnyView(ContentFormView())),
+      Example(name: "Text测试", destination: AnyView(ContentTextView())),
+      Example(name: "导航测试", destination: AnyView(ContentNavView())),
+      Example(name: "按钮测试", destination: AnyView(ContentButtonView())),
+      Example(name: "State 数据", destination: AnyView(ContentStateBindView())),
+      Example(name: "Observ 数据", destination: AnyView(ContentObservView())),
+      Example(name: "ObservableObject 数据", destination: AnyView(ContentStateAcrossView())),
+      Example(name: "StateObject 数据", destination: AnyView(ContentStateObjectView())),
+      Example(name: "Combine 数据", destination: AnyView(ContentCombineView())),
+      Example(name: "Storage 数据", destination: AnyView(ContentStorageView())),
+      Example(name: "ContentObservableObjectView 数据", destination: AnyView(ContentObservableObjectView())),
+  ]
+
+  
+
+struct ContentMainView: View {
+
+      var body: some View {
+          NavigationView {
+
+              List(examples) { example in
+                  NavigationLink(destination: example.destination) {
+                      Text(example.name)
+                  }
+              }
+              .navigationTitle("示例列表")
+          }
+      }
 }
 
 #Preview {
-    MainView()
+  ContentMainView()
 }
