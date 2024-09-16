96a68485eed0eda3293793b39de13d4ab3fabf55
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-09-16 22:56:20 +0800

    feat: 结构整改
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TestSwfitApp/Views/Anmation.swift
@@ -0,0 +1,18 @@
+//
+//  Anmation.swift
+//  TestSwfitApp
+//
+//  Created by L Af on 2024/9/16.
+//
+
+import SwiftUI
+
+struct Anmation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Anmation()
+}
--- TestSwfitApp/Views/ContentView.swift
+++ TestSwfitApp/Views/ContentView.swift
@@ -8,86 +8,21 @@
 import SwiftUI
 
 
-struct Example: Identifiable {
-    let id = UUID()
-    let name: String
-    let destination: AnyView
-}
-
-let examples: [Example] = [
-      Example(name: "网格测试", destination: AnyView(ContentGridView())),
-      Example(name: "表单测试", destination: AnyView(ContentFormView())),
-      Example(name: "Text测试", destination: AnyView(ContentTextView())),
-      Example(name: "导航测试", destination: AnyView(ContentNavView())),
-      Example(name: "按钮测试", destination: AnyView(ContentButtonView())),
-//          Example(name: "UI测试", destination: AnyView(SwiftUIView())),
-      Example(name: "State 数据", destination: AnyView(ContentStateBindView())),
-      Example(name: "Observ 数据", destination: AnyView(ContentObservView())),
-      Example(name: "ObservableObject 数据", destination: AnyView(ContentStateAcrossView())),
-      Example(name: "StateObject 数据", destination: AnyView(ContentStateObjectView())),
-      Example(name: "Combine 数据", destination: AnyView(ContentCombineView())),
-      Example(name: "Storage 数据", destination: AnyView(ContentStorageView())),
-      Example(name: "ContentObservableObjectView 数据", destination: AnyView(ContentObservableObjectView())),
-  ]
-let examplesField: [Example] = [
-//      Example(name: "网格测试", destination: AnyView(FieldContentView())),
-   
-  ]
-  
-
-struct ContentMainView: View {
-
-      var body: some View {
-          NavigationView {
-
-              List(examples) { example in
-                  NavigationLink(destination: example.destination) {
-                      Text(example.name)
-                  }
-              }
-              .navigationTitle("示例列表")
-          }
-      }
-}
-
-// 其他页面的示例
-struct OtherView: View {
-    var body: some View {
-        NavigationView {
-            ScrollView {  // 添加 ScrollView 以允许滚动
-                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3)) {
-                    ForEach(examplesField) { example in
-                        NavigationLink(destination: example.destination) {
-                            VStack {
-                                Text(example.name)
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 100)
-                                    .background(.blue)
-                                    .modifier(BoldAndItalicModifier())
-                                    .foregroundColor(.white)
-                            }
-                        }
-                    }
-                }
-            }
-            .navigationTitle("示例列表") // 如果需要，可以为 NavigationView 添加标题
-        }
-    }
-}
-
 // 主视图，包含底部切换栏
 struct ContentView: View {
     var body: some View {
-        TabView {
-            ContentMainView()
-                .tabItem {
-                    Label("首页", systemImage: "house")
-                }
-            
-            OtherView()
-                .tabItem {
-                    Label("textField", systemImage: "star")
-                }
+      TabView {
+        ContentMainView()
+          .tabItem {
+            Label("首页", systemImage: "text.page").symbolVariant(.fill)
+          }
+        AnmationView()
+          .tabItem {
+            Label("bell", systemImage: "bell").symbolVariant(.fill)
+          }
+        NavComponents().tabItem{
+          Label("组件", systemImage: "lasso" )
+          }
         }
     }
 }
